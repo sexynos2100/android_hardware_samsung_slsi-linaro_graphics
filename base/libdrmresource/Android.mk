3d52c585b1a40a7b0501509d8c63871405e14396
Leo Chen <yinchiuan@google.com>
2023-06-29 06:42:06 +0000

    libhwc2.1: enable thread safety analysis for libdrmresource
    Add -Wthread-safety compilation flag and fix the build break.
    
    Bug: 285487960
    Test: Build pass
    Change-Id: I78ec84779aedfa2b8943c8266d43e7a41d54cbb6
    Signed-off-by: Leo Chen <yinchiuan@google.com>

--- base/libdrmresource/Android.mk
+++ base/libdrmresource/Android.mk
@@ -41,6 +41,7 @@ LOCAL_SRC_FILES := \
 
 LOCAL_CFLAGS := -DHLOG_CODE=0
 LOCAL_CFLAGS += -Wno-unused-parameter
+LOCAL_CFLAGS += -Wthread-safety
 LOCAL_EXPORT_SHARED_LIBRARY_HEADERS := libdrm
 
 LOCAL_MODULE := libdrmresource
