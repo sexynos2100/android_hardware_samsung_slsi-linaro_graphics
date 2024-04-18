d2f72ced6f658bae01337f76bfb29275cf3f2453
oberdfr <alebennella@gmail.com>
2024-04-18 10:26:32 +0200

    graphics: exynos9810: libhwc2.1: Configure brightness nodes

--- exynos9810/libhwc2.1/ExynosHWCModule.h
+++ exynos9810/libhwc2.1/ExynosHWCModule.h
@@ -27,6 +27,8 @@
 #define DP_LINK_NAME  "11090000.displayport"
 #define DP_UEVENT_NAME  "change@/devices/platform/%s/extcon/extcon0"
 #define DP_CABLE_STATE_NAME "/sys/devices/platform/%s/extcon/extcon0/cable.%d/state"
+#define BRIGHTNESS_NODE_BASE "/sys/class/backlight/panel/brightness"
+#define MAX_BRIGHTNESS_NODE_BASE "/sys/class/backlight/panel/max_brightness"
 #define USE_DPU_SET_CONFIG
 
 #define WAIT_FENCE  1
